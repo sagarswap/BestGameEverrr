12b89d9826bf310289ed3ad5f9784de879cb6c94
sagarswap <sagarswap@gmail.com>
2023-12-28 18:27:48 +0530

    Forked to primitieve Game

--- src/PrimitiveGame.java
+++ src/PrimitiveGame.java
@@ -1,11 +1,24 @@
 import java.io.*;
 
 public class PrimitiveGame {
-    int difficulty;
+    int difficulty, steps=0;
+    
     public static void main(String[] args) throws IOException{
-        System.out.print("Please Enter Difficulty: (1- Easy, 2-Medium, 3-Hard)");
+        System.out.print("Please Enter Difficulty: (1- Easy, 2-Medium, 3-Hard) ");
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int diff=Integer.parseInt(br.readLine());
         Minesweeper minesweeper=new Minesweeper(diff);
+        PrimitiveGame game=new PrimitiveGame();
+        game.playGame();
+    }
+
+    private void playGame() throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        for(;;){
+            System.out.println("Enter Row and Col:");
+            int row=Integer.parseInt(br.readLine());
+            int col=Integer.parseInt(br.readLine());
+            
+        }
     }
 }
